1c6736d7daad041052839504e9cdf774f031b4cb
MoisesMed <br.moises@hotmail.com>
2021-04-26 16:10:49 -0300

    copy

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './home.css'
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -17,7 +17,21 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 export default function Home() {
+    const inputRef = useRef();
+
+    function copy() {
+        if (!inputRef.current) return false;
+
+        inputRef.current.select();
+        document.execCommand("copy");
+        setCor("primary")
+        setButton("COPY!")
+    }
     const classes = useStyles();
+    const [ivalue, setIvalue] = React.useState("teste")
+    const [cor, setCor] = React.useState("default")
+    const [button, setButton] = React.useState("CLICK HERE!")
+    const [value, setValue] = React.useState(null)
 
     return (
         <Container className="Body" maxWidth="md">
@@ -25,15 +39,20 @@ export default function Home() {
             <div className="InputUrl">
                 <div>Paste your URL here</div>
                 <div style={{ display: 'flex', width: 'auto', marginTop: 10, marginBottom: 10 }}>
-                    <TextField
+                    <input
                         label="URL here"
-                        style={{ width: '70%', height: 'auto', backgroundColor: 'white' }}
-                        id="filled-margin-none"
+                        style={{ width: '70%', height: '40px', border: "1px solid gray", borderRadius: 5, paddingLeft: 10 }}
+                        id="fi"
+                        ref={inputRef}
+                        type="text"
                         fullWidth
+                        onChange={e => setValue(e.target.value)}
                         className={classes.textField}
                         variant="outlined"
                     />
-                    <Button color="primary" variant="contained" style={{ width: '30%', height: 'auto' }}>CLICK HERE!</Button>
+                    <Button
+                        onClick={copy}
+                        color={cor} variant="contained" style={{ width: '30%', height: 'auto' }}>{button}</Button>
                 </div>
                 <div>Shortening will help you to decrease the size of your URLs</div>
             </div>
@@ -41,6 +60,6 @@ export default function Home() {
             <p className="desc">CUT is a URL shortener, it takes your URL and shortens it to make it easier to remember or send to anyone.</p>
             <h2 className="title">How to use?</h2>
             <p className="desc">Just enter your URL and click on the button, it will be shortened afterwards and you will be able to copy</p>
-        </Container>
+        </Container >
     );
 }
