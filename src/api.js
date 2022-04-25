38cd3bdb97dd84503d0ec419dbc1efabe5e6b8c7
MoisesMed <br.moises@hotmail.com>
2022-04-25 09:14:07 -0300

    teste no ar

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "https://cors-anywhere.herokuapp.com/"+"https://api.encurtador.dev"
+const API_URL = "https://api.encurtador.dev"
 axios.defaults.baseURL = API_URL
 
 export const api = axios.create({
